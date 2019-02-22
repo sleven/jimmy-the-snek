994387a3ab6e664ac89e536dc4b1e8853b91ddce
gserrao <gserrao@sourcefire.com>
2019-02-22 14:13:18 -0500

    Made bubble around head

--- app/main.py
+++ app/main.py
@@ -90,7 +90,6 @@ def move():
     board[1][current_y] = True
 
     direction = 'down'
-    #comment
     if current_x + 1 < len(board[0]) and last_direction != 'left':
         if not board[0][current_x + 1]:
             direction = 'right'
